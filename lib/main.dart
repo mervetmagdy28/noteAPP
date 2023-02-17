47bc8e3c8e7cfb3e0ffae7daeca5d0346bbc188e
Mervet_Magdy <mervetmagdy28@gmail.com>
2023-02-18 00:15:11 +0200

    add note model generator

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class NotesApp extends StatelessWidget {
      return MaterialApp(
        debugShowCheckedModeBanner: false,
        theme: ThemeData( brightness: Brightness.dark ,fontFamily: 'Poppins'),
+
        home: const NotesView(),
      );
   }
